411f7e1a137640318c9fb28ad176857a3b7ae587
samxzhk <samiacunhaa004@gmail.com>
2023-06-14 23:00:18 -0300

    Mobile version almost finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const toggleLine = document.querySelectorAll('.toggle__line');
 const toggleIcon = document.getElementById('toggle-icon');
 const navMenu = document.getElementById('nav-menu');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
 function toggleIconFunc()
 {
     toggleLine.forEach(x => x .classList.toggle('toggle-clicked'));
@@ -10,3 +12,37 @@ function toggleIconFunc()
 
 
 toggleIcon.addEventListener('click', toggleIconFunc);
+
+
+let slideNum = 0;
+/* ====== SLIDER ============== */
+const sliderImages = document.querySelectorAll('.slider__img-img');
+
+function slideNext()
+{
+    sliderImages.forEach((slide) =>
+    {
+        slide.classList.remove('active');
+    })
+    slideNum+=1;
+    if(slideNum > (sliderImages.length -1))
+    {
+        slideNum = 0;
+    }
+    sliderImages[slideNum].classList.add('active');
+}
+function slidePrevious()
+{
+    sliderImages.forEach((slide) =>
+    {
+        slide.classList.remove('active');
+    })
+    slideNum-=1;
+    if(slideNum < 0)
+    {
+        slideNum = sliderImages.length -1;
+    }
+    sliderImages[slideNum].classList.add('active');
+}
+nextButton.addEventListener('click', slideNext);
+prevButton.addEventListener('click', slidePrevious);
